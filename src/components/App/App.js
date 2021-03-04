b126be92baed91e66b358f5641719f6986a1f92a
Richard Tyler <richardtyler2011@gmail.com>
2021-03-03 19:21:46 -0700

    Move footer its own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,7 @@ class App extends Component {
         <Header />
         <main>
         </main>
-        <footer></footer>
+        <Footer />
       </div>
     )
   }
