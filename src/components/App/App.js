8f44ff23c8b130b35d286edd58951107f70464ff
Richard Tyler <richardtyler2011@gmail.com>
2021-03-03 22:18:53 -0700

    Style knock button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,13 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <main>
+        <main className="main">
+          <h2>RULES</h2>
+          <section  className="rules-container">
+            <h3>You will be given a quote from a character in Breaking Bad and three characters.</h3>
+            <h4>Choose the character that you think authored the quote.</h4> 
+          </section>
+          <button className="knock-button">Be the one who knocks</button>
         </main>
         <Footer />
       </div>
