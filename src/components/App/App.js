235b4a3c1082d05602efb25d98263e1126f28791
Richard Tyler <richardtyler2011@gmail.com>
2021-03-03 22:27:15 -0700

    CHANGE BUTTON TO SAY KNOCK

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,7 +19,7 @@ class App extends Component {
             <h3>You will be given a quote from a character in Breaking Bad and three characters.</h3>
             <h4>Choose the character that you think authored the quote.</h4> 
           </section>
-          <button className="knock-button">Be the one who knocks</button>
+          <button className="knock-button">KNOCK</button>
         </main>
         <Footer />
       </div>
