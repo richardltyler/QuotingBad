a6d8fe369c55ae033b94f7520b1221f26de26eeb
Richard Tyler <richardtyler2011@gmail.com>
2021-03-04 17:02:41 -0700

    Add u to span in logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
         <h1 className="logo">
-          <span className='logo-block'>Q</span>uoting<br></br>
+          <span className='logo-block'>Qu</span>oting<br></br>
           <span className='logo-block'>Ba</span>d
         </h1>
     </header>
