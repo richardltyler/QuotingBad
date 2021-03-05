571d3cdafa6bd54fa8c228e99ba7b0e09172b6e0
Richard Tyler <richardtyler2011@gmail.com>
2021-03-04 21:38:40 -0700

    Link knock button to Game component

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Start.css';
 
 const Start = (props) => {
@@ -9,7 +10,9 @@ const Start = (props) => {
       <h2 className="headline" >RULES</h2>
       <h3>You will be given a quote from a character in Breaking Bad and three characters.</h3>
       <h4>Choose the character that you think authored the quote.</h4> 
+      <Link to='/game'>
       <button onClick={() => showQuote()} className ="knock-button hover-states">KNOCK</button>
+      </Link>
     </section>
   )
 }
