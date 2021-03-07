8634d3b9d2e0a679ab7179890094410d63c32e43
Richard Tyler <richardtyler2011@gmail.com>
2021-03-07 11:41:21 -0700

    Test header

---
+++ cypress/integration/header_spec.js
@@ -0,0 +1,19 @@
+describe('Header Component', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/#/')
+  });
+
+  it('Should have a logo that says Quoting Bad', () => {
+    cy.get('h1').get('span:first').should('contain', 'Qu');
+    cy.get('h1').get('span:last').should('contain', 'Ba');
+  }) 
+
+  it('Should not have a home button', () => {
+    cy.get('header').should('not.contain', '.home-link')
+  });
+
+  it.only('Should have a home button when on the about page', () => {
+    cy.visit('http://localhost:3000/#/about')
+      .get('header a').should('have.class', 'home-link');
+  })
+})
