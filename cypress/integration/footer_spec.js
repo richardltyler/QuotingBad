e07bb8e95ff608eea1fb184900450d9d5fcbf7a9
Richard Tyler <richardtyler2011@gmail.com>
2021-03-08 11:57:51 -0700

    Test header and footer at all urls

--- cypress/integration/footer_spec.js
+++ cypress/integration/footer_spec.js
@@ -12,8 +12,11 @@ describe('Footer Component', () => {
     cy.url().should('contain', '/about');
   });
 
-  it('Should be visible on the about page', () => {
-    cy.get('footer a').click();
-    cy.get('footer a').should('contain', 'About');
+  it('Should be visible on every url', () => {
+    cy.visit('http://localhost:3000/#/error')
+      .get('footer').should('be.visible');
+    
+    cy.visit('http://localhost:3000/#/about')
+      .get('footer').should('be.visible');
   });
 })
--- cypress/integration/header_spec.js
+++ cypress/integration/header_spec.js
@@ -15,5 +15,13 @@ describe('Header Component', () => {
   it('Should have a home button when on the about page', () => {
     cy.visit('http://localhost:3000/#/about')
       .get('header a').should('have.class', 'home-link');
-  })
+  });
+
+  it('Should be visible on every url', () => {
+    cy.visit('http://localhost:3000/#/error')
+      .get('header').should('be.visible');
+    
+    cy.visit('http://localhost:3000/#/about')
+      .get('header').should('be.visible');
+  });
 })
