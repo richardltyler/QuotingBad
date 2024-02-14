fac12bd48ff18be1f98e753c9541461a0c8c3acf
Richard Tyler <richardtyler2011@gmail.com>
2024-02-14 14:29:09 -0600

    Updates

--- src/httpRequests.js
+++ src/httpRequests.js
@@ -1,24 +1,22 @@
 const httpRequests = {
   getAllQuotes() {
-    return (
-      fetch('https://www.breakingbadapi.com/api/quotes')
-      .then(res => res.json())
-    )
+    return fetch("https://api.breakingbadquotes.xyz/v1/quotes/10").then((res) =>
+      res.json()
+    );
   },
   getCharacters(character) {
-    return (
-      fetch(`https://www.breakingbadapi.com/api/characters?name=${character}`)
-      .then(res => res.json())
-        .then(characters => {
-          if (characters.length === 0) {
-            return 'error';
-            
-          } else {
-            return characters[0].img;
-          }
-        })
+    return fetch(
+      `https://www.breakingbadapi.com/api/characters?name=${character}`
     )
-  }
-}
+      .then((res) => res.json())
+      .then((characters) => {
+        if (characters.length === 0) {
+          return "error";
+        } else {
+          return characters[0].img;
+        }
+      });
+  },
+};
 
 export default httpRequests;
